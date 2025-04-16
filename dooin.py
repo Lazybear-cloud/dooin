3bac12851b9a4ddddbd305f95c4c1077b6b4f867
Lazybear-cloud <wolfht93@gmail.com>
2025-04-16 16:10:03 +0900

    Update dooin.py

--- dooin.py
+++ dooin.py
@@ -1,165 +1,126 @@
-import selenium
 import time
-import requests
-import pandas
+import pandas as pd
 from bs4 import BeautifulSoup
+from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import os
 
-driver_path = "C:/chromedriver.exe"  # 드라이버 경로
-service = Service(driver_path)  # Service 객체 생성
-driver = webdriver.Chrome(service=service)  # service 인자 사용
+def run_crawler():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')
 
-url = 'https://www.dooinauction.com/'
-driver.get(url)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-#로그인 버튼 클릭
-driver.find_element(By.XPATH, '//*[@id="header"]/div[1]/div[1]/div[1]/span[3]').click()
-time.sleep(2)
+    url = 'https://www.dooinauction.com/'
+    driver.get(url)
 
-#ID, PW 입력
-login_ID = driver.find_element(By.XPATH, '//*[@id="client_id"]')
-login_ID.send_keys('wolfht93')
-
-
-login_ID = driver.find_element(By.XPATH, '//*[@id="passwd"]')
-login_ID.send_keys('07llawliet!')
-
-
-driver.find_element(By.XPATH, '//*[@id="loginUI"]/tbody/tr[5]/td/a').click()
-time.sleep(2)
-
-#원하는 메뉴 들어가기
-driver.find_element(By.XPATH, '//*[@id="header"]/div[1]/div[1]/div[1]/span[3]').click()
-time.sleep(1)
-
-driver.find_element(By.XPATH, '//*[@id="aside"]/div[1]/div/ul/li[3]/a').click()
-time.sleep(1)
-
-driver.find_element(By.XPATH, '//*[@id="bookmarkListContent01"]/div/div/div/div[1]/div[1]/a/strong').click()
-time.sleep(1)
-
-data_frame = pandas.DataFrame([])
-
-for m in range(2,100):
-    for n in range(1,21):
-        #목록에서 n번째 물건 클릭
-        driver.find_element(By.XPATH, '//*[@id="Tr_{}"]/td[3]/div[4]'.format(n)).click()
-        time.sleep(1)
-    
-        #새로 열린 페이지 선택
-        driver.switch_to.window(driver.window_handles[1])
-        time.sleep(1)
-    
-        # 현재 페이지 HTML 가져오기
-        html = driver.page_source
-        
-        # BeautifulSoup으로 HTML 파싱
-        soup = BeautifulSoup(html, "html.parser")
-    
-        # 페이지의 모든 텍스트 가져오기
-        text = soup.get_text(separator="\n", strip=True)
-    
-        
-        #사건번호 추출
-        try:
-            title = soup.find("span", class_="f20 bold_900").text
-            
-        except:
-            title = 'error'
-    
-        
-        #물건 종류 추출
-        try:
-            target_th = soup.find("th", string="물건종류")
-            property_type = target_th.find_next_sibling("td").text
-    
-        except:
-            property_type = 'error'
-    
-        
-        #도/시, 시/구, 구/동 추출
-        try:
-            address = driver.find_element(By.XPATH, '//*[@id="lyCnt_base"]/table/tbody/tr[1]/td/span[1]').text.strip()
-            address = address.split()
-            address1 = address[0] #도/시
-            address2 = address[1] #시/구
-            address3 = address[2] #구/동
-    
-        except:
-            address1 = 'error'
-            address2 = 'error'
-            address3 = 'error'
-        
-    
-        #층 추출
-        try:
-            if isinstance(address, list):
-                address = " ".join(address)
-            
-            if any(keyword in address for keyword in ["지하", "지층", "지하층"]):
-                floor = "반지하"
-                
-            else:
-                address = driver.find_element(By.XPATH, '//*[@id="lyCnt_base"]/table/tbody/tr[1]/td/span[1]').text.strip().split()
-                for i, floor in enumerate(address):
-                    if "층" in floor:
-                        break
-                        
-                position = floor.find("층")
-                floor = floor[:position + 1]
-    
-        except:
-            floor = 'error'
-    
-        #사용승인 추출
-        try:
-            approval = soup.find("span", string=lambda text: text and "사용승인일" in text)
-            approval = approval.text.strip().split(':')
-            approval = approval[1].split('-')[0]+'년'
-    
-        except:
-            approval = 'error'
-    
-        #감정가, 최저가, 퍼센트 추출
-        try:
-            og_price = driver.find_element(By.XPATH, '//*[@id="lyCnt_base"]/table/tbody/tr[3]/td[3]/span').text.strip()
-            law_price = driver.find_element(By.XPATH, '//*[@id="lyCnt_base"]/table/tbody/tr[4]/td[3]/span').text.strip().split()[1]
-            percent = driver.find_element(By.XPATH, '//*[@id="lyCnt_base"]/table/tbody/tr[4]/td[3]/span').text.strip().split()[0].split('(')[1].split(')')[0]
-    
-        except:
-            og_price = 'error'
-            law_price = 'error'
-            percent = 'error'
-
-        #근생 여부
-        try:
-            keywords = ["사무소", "사무실", "의원", "한의원", "공인중개사", "소매점", "근생빌라", "현황 주거용", "현황 주택", "현황 도시형생활주택", "현황:다세대주택", "현황:도시형생활주택"]
-            special_type = "근생빌라" if any(keyword in text for keyword in keywords) else ""
-
-        except:
-            special_type = 'error'
-
-        
-        print(f'{(m-2)*20 + n}번째 물건, {title}, {property_type}, {address1}, {address2}, {address3}, {floor}, {approval}, {og_price}, {law_price}, {percent}, {special_type}')
-
-        new_data_frame = pandas.DataFrame([[title, property_type, address1, address2, address3, floor, approval, og_price, law_price, percent, special_type]], columns = ['사건 번호', '종류', '도/시', '시/구', '구/동', '층', '사용승인', '감정가', '최저가', '퍼센트', '근생빌라'])
-        
-        data_frame = pandas.concat([data_frame, new_data_frame],axis=0)
-                        
-        driver.close()
-        time.sleep(1)
-    
-        driver.switch_to.window(driver.window_handles[0])
-        time.sleep(1)
-
-    #다음 페이지로 넘어가기
-    driver.find_element(By.XPATH, '//*[@id="loadPage_{}"]'.format(m)).click()
+    driver.find_element(By.XPATH, '//*[@id="header"]/div[1]/div[1]/div[1]/span[3]').click()
     time.sleep(2)
 
-data_frame
+    login_ID = driver.find_element(By.XPATH, '//*[@id="client_id"]')
+    login_ID.send_keys('wolfht93')
 
-data_frame.to_excel('D:\임형태\dooin0220.xlsx', index = False)
+    login_PW = driver.find_element(By.XPATH, '//*[@id="passwd"]')
+    login_PW.send_keys('07llawliet!')
+
+    driver.find_element(By.XPATH, '//*[@id="loginUI"]/tbody/tr[5]/td/a').click()
+    time.sleep(2)
+
+    driver.find_element(By.XPATH, '//*[@id="header"]/div[1]/div[1]/div[1]/span[3]').click()
+    time.sleep(1)
+
+    driver.find_element(By.XPATH, '//*[@id="aside"]/div[1]/div/ul/li[3]/a').click()
+    time.sleep(1)
+
+    driver.find_element(By.XPATH, '//*[@id="bookmarkListContent01"]/div/div/div/div[1]/div[1]/a/strong').click()
+    time.sleep(1)
+
+    data_frame = pd.DataFrame([])
+
+    for m in range(2, 3):
+        for n in range(1, 3):
+            try:
+                driver.find_element(By.XPATH, f'//*[@id="Tr_{n}"]/td[3]/div[4]').click()
+                time.sleep(1)
+
+                driver.switch_to.window(driver.window_handles[1])
+                time.sleep(1)
+
+                html = driver.page_source
+                soup = BeautifulSoup(html, "html.parser")
+                text = soup.get_text(separator="\n", strip=True)
+
+                try:
+                    title = soup.find("span", class_="f20 bold_900").text
+                except:
+                    title = 'error'
+
+                try:
+                    target_th = soup.find("th", string="물건종류")
+                    property_type = target_th.find_next_sibling("td").text
+                except:
+                    property_type = 'error'
+
+                try:
+                    address = driver.find_element(By.XPATH, '//*[@id="lyCnt_base"]/table/tbody/tr[1]/td/span[1]').text.strip().split()
+                    address1, address2, address3 = address[:3]
+                except:
+                    address1 = address2 = address3 = 'error'
+
+                try:
+                    if isinstance(address, list):
+                        address_text = " ".join(address)
+                        floor = "반지하" if any(k in address_text for k in ["지하", "지층", "지하층"]) else next((s for s in address if "층" in s), 'error')
+                    else:
+                        floor = 'error'
+                except:
+                    floor = 'error'
+
+                try:
+                    approval = soup.find("span", string=lambda t: t and "사용승인일" in t).text.strip().split(':')[1].split('-')[0] + '년'
+                except:
+                    approval = 'error'
+
+                try:
+                    og_price = driver.find_element(By.XPATH, '//*[@id="lyCnt_base"]/table/tbody/tr[3]/td[3]/span').text.strip()
+                    law_price = driver.find_element(By.XPATH, '//*[@id="lyCnt_base"]/table/tbody/tr[4]/td[3]/span').text.strip().split()[1]
+                    percent = driver.find_element(By.XPATH, '//*[@id="lyCnt_base"]/table/tbody/tr[4]/td[3]/span').text.strip().split()[0].split('(')[1].split(')')[0]
+                except:
+                    og_price = law_price = percent = 'error'
+
+                try:
+                    keywords = ["사무소", "사무실", "의원", "한의원", "공인중개사", "소매점", "근생빌라", "현황 주거용", "현황 주택", "현황 도시형생활주택", "현황:다세대주택", "현황:도시형생활주택"]
+                    special_type = "근생빌라" if any(k in text for k in keywords) else ""
+                except:
+                    special_type = 'error'
+
+                new_row = pd.DataFrame([[title, property_type, address1, address2, address3, floor, approval, og_price, law_price, percent, special_type]],
+                                       columns=['사건 번호', '종류', '도/시', '시/구', '구/동', '층', '사용승인', '감정가', '최저가', '퍼센트', '근생빌라'])
+                data_frame = pd.concat([data_frame, new_row], axis=0)
+
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
+                time.sleep(1)
+
+            except Exception as e:
+                print(f"Error at page {m}, item {n}: {e}")
+
+        try:
+            driver.find_element(By.XPATH, f'//*[@id="loadPage_{m}"]').click()
+            time.sleep(2)
+        except:
+            print(f"페이지 {m} 없음")
+            break
+
+    os.makedirs('data', exist_ok=True)
+    data_frame.to_excel('data/dooin_result.xlsx', index=False)
+    driver.quit()
+
+if __name__ == '__main__':
+    run_crawler()
